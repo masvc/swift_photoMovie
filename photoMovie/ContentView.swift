8354474df633dbf4c0c06afca2f19c500fda1a64
mas <m.yoshida553@gmail.com>
2025-02-16 17:05:34 +0900

    Initial Commit

---
+++ photoMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photoMovie
+//
+//  Created by masato yoshida on 2025/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photoMovie/photoMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoMovieApp.swift
+//  photoMovie
+//
+//  Created by masato yoshida on 2025/02/16.
+//
+
+import SwiftUI
+
+@main
+struct photoMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
